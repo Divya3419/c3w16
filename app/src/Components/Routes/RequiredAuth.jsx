727b6cde86440d98abce963928826ba6bbc16694
Divya <99958902+Divya3419@users.noreply.github.com>
2022-05-06 09:28:53 +0530

    done

--- app/src/Components/Routes/RequiredAuth.jsx
+++ app/src/Components/Routes/RequiredAuth.jsx
@@ -1,10 +1,10 @@
-import { useContext,useLocation } from "react"
+import { useContext } from "react"
 import { Link} from "react-router-dom"
 import { AuthContext } from "../context/AuthContext"
 
 export const RequiredAuth = ({children})=>{
 const {token}=useContext(AuthContext);
-const location=useLocation();
+//const location=useLocation();
 if(token) return children;
     return <h3>Please <Link to = "/login">login</Link> to access this page</h3>
 }
